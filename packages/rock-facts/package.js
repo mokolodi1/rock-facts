a6e8b7fb5e596f1d6165fa0aa793505b79456899
mokolodi1 <mokolodi1@gmail.com>
2015-11-02 11:04:28 -0800

    preparing for publishing

--- packages/rock-facts/package.js
+++ packages/rock-facts/package.js
@@ -2,9 +2,9 @@ Package.describe({
   name: 'mokolodi1:rock-facts',
   version: '0.0.1',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'Improvements to sAlert',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'https://github.com/mokolodi1/rock-facts/',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
